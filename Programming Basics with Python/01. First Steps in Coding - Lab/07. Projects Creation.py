0bbd9225129e04b6f958f83d15e1ec6907db77c9
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-05 10:18:05 +0200

    Create 07. Projects Creation.py

---
+++ Programming Basics with Python/01. First Steps in Coding - Lab/07. Projects Creation.py
@@ -0,0 +1,6 @@
+architecture_name = input()
+num_of_projects_to_be_done = int(input())
+hours_for_one_project = 3
+time_for_projects_to_be_done = num_of_projects_to_be_done * hours_for_one_project
+print (f'The architect {architecture_name}'
+       f' will need {time_for_projects_to_be_done} hours to complete {num_of_projects_to_be_done} project/s.')
