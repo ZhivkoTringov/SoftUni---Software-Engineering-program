16461bd85a50d120726f37a8d94580a1b3c3950b
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-05 10:18:19 +0200

    Create 08. Pet Shop.py

---
+++ Programming Basics with Python/01. First Steps in Coding - Lab/08. Pet Shop.py
@@ -0,0 +1,8 @@
+num_dog_food = int(input())
+num_cat_food = int(input())
+package_dog_food = 2.5
+package_cat_food = 4
+dog_foo_price = num_dog_food *package_dog_food
+cat_food_price = num_cat_food * package_cat_food
+total_sum = dog_foo_price + cat_food_price
+print(f'{total_sum} lv.')
