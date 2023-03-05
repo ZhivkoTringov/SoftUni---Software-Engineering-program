0e317ba8978922c2b79e0577b9d96dd4b8b19e38
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-05 10:37:54 +0200

    Create 08. Basketball Equipment.py

---
+++ Programming Basics with Python/01. First Steps in Coding - Exercise/08. Basketball Equipment.py
@@ -0,0 +1,7 @@
+annual_tax = int(input())
+sneakers = annual_tax * 0.6
+suit = sneakers * 0.8
+ball = suit / 4
+accessories = ball / 5
+total_sum = annual_tax + sneakers + suit + ball + accessories
+print(total_sum)
