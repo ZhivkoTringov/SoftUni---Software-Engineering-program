28567555c75122e7103a31e9ad911857636b9662
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-05 18:09:14 +0200

    Solution to tasks.

---
+++ Programming Basics with Python/02. Conditional Statements - Lab/04. Password Guess.py
@@ -0,0 +1,5 @@
+user_pass = input()
+if user_pass == "s3cr3t!P@ssw0rd":
+    print('Welcome')
+else:
+    print('Wrong password!')
---
+++ Programming Basics with Python/02. Conditional Statements - Lab/07. Area of Figures.py
@@ -0,0 +1,20 @@
+from math import pi
+
+
+type = input()
+area = 0
+if type == 'square':
+    a = float(input())
+    area = a **2
+elif type == 'rectangle':
+    a = float(input())
+    b = float(input())
+    area = a * b
+elif type == 'circle':
+    r = float(input())
+    area = pi * r ** 2
+elif type == 'triangle':
+    a = float(input())
+    h = float(input())
+    area = a * h / 2
+print(f'{area:.3f}')
