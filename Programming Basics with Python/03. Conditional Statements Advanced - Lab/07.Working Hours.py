3ced52a9d560adb5f7ab5f9761cacb20d0fe0d1a
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-07 20:03:39 +0200

    Create 07.Working Hours.py

---
+++ Programming Basics with Python/03. Conditional Statements Advanced - Lab/07.Working Hours.py
@@ -0,0 +1,8 @@
+hour = int(input())
+day_of_week = input()
+if day_of_week in 'Monday Tuesday Wednesday Thursday Friday Saturday':
+    if 10 <= hour <=18:
+        print('open')
+    else: print('closed')
+else:
+    print('closed')
