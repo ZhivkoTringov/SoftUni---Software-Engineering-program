c39a283f374d65961d0659f332cf66bfc9f299a8
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-16 13:11:07 +0200

    Create 05. Travelling.py

---
+++ Programming Basics with Python/06. Nested Loops - Lab/05. Travelling.py
@@ -0,0 +1,10 @@
+destination = input()
+total = 0
+while destination != 'End':
+    needed_money = float(input())
+    total = 0
+    while total < needed_money:
+        saved_money = float(input())
+        total += saved_money
+    print(f'Going to {destination}!')
+    destination = input()
