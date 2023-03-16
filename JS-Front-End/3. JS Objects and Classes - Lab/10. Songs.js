adae0401687d050e255b686558435751ac8c492e
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-16 14:35:28 +0200

    Create 10. Songs.js

---
+++ JS-Front-End/3. JS Objects and Classes - Lab/10. Songs.js
@@ -0,0 +1,27 @@
+function solve(input){
+    class Song {
+        constructor(type, name, time){
+            this.type = type;
+            this.name = name;
+            this.time = time;
+        }
+
+    }
+
+    let songs = [];
+    let numberOfSong = input.shift();
+    let typeOfSong = input.pop();
+
+    for (let i = 0; i < numberOfSong; i++) {
+        let [type, name, time] = input[i].split('_');
+        let song = new Song(type, name, time);
+        songs.push(song);
+    }
+
+    if (typeOfSong === 'all') {
+        songs.forEach((i) => console.log(i.name));
+    } else {
+        let filtered = songs.filter((i) => i.type === typeOfSong);
+        filtered.forEach((i) => console.log(i.name));
+    }
+}
