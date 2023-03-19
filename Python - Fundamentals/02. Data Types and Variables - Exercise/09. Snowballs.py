1b87c72ad6efd65b3df057cff3839ffdf43ee766
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-19 22:05:57 +0200

    Create 09. Snowballs.py

---
+++ Python - Fundamentals/02. Data Types and Variables - Exercise/09. Snowballs.py
@@ -0,0 +1,16 @@
+num_of_snowballs = int(input())
+max_snowball_weight = 0
+max_snowball_time = 0
+max_snowball_quality = 0
+max_snowball_value = 0
+for i in range (num_of_snowballs):
+    snowball_weight = int(input())
+    snowball_time = int(input())
+    snowball_quality = int(input())
+    snowball_value = (snowball_weight / snowball_time) ** snowball_quality
+    if snowball_value > max_snowball_value:
+        max_snowball_value = snowball_value
+        max_snowball_weight = snowball_weight
+        max_snowball_time = snowball_time
+        max_snowball_quality = snowball_quality
+print(f'{max_snowball_weight} : {max_snowball_time} = {int(max_snowball_value)} ({max_snowball_quality})')
