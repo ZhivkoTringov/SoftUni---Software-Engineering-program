59d95636ec6ccbfbd71378e99d6da07442f18105
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-22 23:44:21 +0200

    02. Show More

---
+++ JS-Front-End/04. Dom and Events - Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let href = document.getElementById('more');
+    let span = document.getElementById('text');
+
+    href.style.display = 'none';
+    span.style.display = 'inline';
+}
