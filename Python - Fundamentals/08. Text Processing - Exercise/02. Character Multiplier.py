7cd40d82a0c0388c42a122dcb0f9f98f8154fc82
Zhivko Tringov <zhivkotringov@gmail.com>
2023-03-31 23:30:58 +0300

    Create 02. Character Multiplier.py

---
+++ Python - Fundamentals/08. Text Processing - Exercise/02. Character Multiplier.py
@@ -0,0 +1,18 @@
+data = input().split(' ')
+first_string = data[0]
+second_string = data[1]
+total_sum = 0
+if len(first_string) > len(second_string):
+    for index in range(len(second_string)):
+        total_sum += ord(first_string[index]) * ord(second_string[index])
+    for index in range(len(second_string), len(first_string)):
+        total_sum += ord(first_string[index])
+elif len(second_string) > len(first_string):
+    for index in range(len(first_string)):
+        total_sum += ord(first_string[index]) * ord(second_string[index])
+    for index in range(len(first_string), len(second_string)):
+        total_sum += ord(second_string[index])
+elif len(first_string) == len(second_string):
+    for index in range(len(first_string)):
+        total_sum += ord(first_string[index]) * ord(second_string[index])
+print(total_sum)
