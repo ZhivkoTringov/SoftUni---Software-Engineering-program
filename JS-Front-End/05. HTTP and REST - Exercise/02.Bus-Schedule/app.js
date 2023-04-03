dac899c5a02e75169b6e3744a0605f8077f2551e
Zhivko Tringov <zhivkotringov@gmail.com>
2023-04-03 12:25:43 +0300

    02.Bus-Schedule

---
+++ JS-Front-End/05. HTTP and REST - Exercise/02.Bus-Schedule/app.js
@@ -0,0 +1,39 @@
+function solve() {
+  const busContainer = document.querySelector("#info > span");
+  const departBtn = document.getElementById("depart");
+  const arriveBtn = document.getElementById("arrive");
+  const BASE_URL = "http://localhost:3030/jsonstore/bus/schedule/";
+  let nextStop = "depot";
+  let stopName = null;
+
+  function depart() {
+    arriveBtn.disabled = false;
+    departBtn.disabled = true;
+    fetch(`${BASE_URL}${nextStop}`)
+      .then((res) => res.json())
+      .then((nextStopInfo) => {
+        const {name, next} = nextStopInfo;
+        busContainer.textContent = `Next stop ${name}`
+        nextStop = next;
+        stopName = name;
+      })
+      .catch(() => {
+        busContainer.textContent = "Error";
+        arriveBtn.disabled = true;
+        departBtn.disabled = true;
+      });
+  }
+
+  async function arrive() {
+    arriveBtn.disabled = true;
+    departBtn.disabled = false;
+    busContainer.textContent = `Arriving at ${stopName}`;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
+}
+
+let result = solve();
