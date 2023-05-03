981c68b92f3a2ac9b43c40d95a5c316ac8aa0086
Zhivko Tringov <zhivkotringov@gmail.com>
2023-05-03 21:27:43 +0300

    Create 01. Take Skip.py

---
+++ Python OOP/Iterators and Generators/01. Take Skip.py
@@ -0,0 +1,16 @@
+class take_skip:
+
+    def __init__(self, step, count):
+        self.step = step
+        self.count = count
+        self.passed_iterations = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.passed_iterations == self.count:
+            raise StopIteration
+        current_number = self.passed_iterations * self.step
+        self.passed_iterations += 1
+        return current_number
